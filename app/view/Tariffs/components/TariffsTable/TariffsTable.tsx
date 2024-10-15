861c09346b6c6b70f9a52e9689074f1064b86e71
Ivan Bieliakov <ivan.beliakov@data-ox.com>
2024-10-15 13:52:01 +0300

    responsive

--- app/view/Tariffs/components/TariffsTable/TariffsTable.tsx
+++ app/view/Tariffs/components/TariffsTable/TariffsTable.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Container } from "@/app/components";
-import { FC, useMemo, useState } from "react";
+import { FC, use, useCallback, useEffect, useMemo, useState } from "react";
 
 import { useTranslations } from "next-intl";
 import Image from "next/image";
@@ -21,329 +21,338 @@ const TariffsTable: FC<Props> = ({ isBlack = false }) => {
     "limits_replenishment",
     "limits_withdrawal",
   ].map((header) => t(header));
-  const tableContent = [
-    {
-      key: "usdt",
-      assets: (
-        <div className={styles.assets}>
-          <div className={classNames(styles.text, styles.subText)}>
-            {t("assets")}
+  const tableContent = useMemo(
+    () => [
+      {
+        key: "usdt",
+        assets: (
+          <div className={styles.assets}>
+            <div className={classNames(styles.text, styles.subText)}>
+              {t("assets")}
+            </div>
+            <b>USDT </b>Tether
           </div>
-          <b>USDT </b>Tether
-        </div>
-      ),
-      contents: [
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/usdt.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />{" "}
-            <span>
-              <b>USDT</b> <span className={styles.no_break}>(ERC-20)</span>
-            </span>
-          </div>,
-          "1 USDT",
-          "25 USDT",
-          "min 1 USDT",
-          "min 10 USDT",
+        ),
+        contents: [
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/usdt.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />{" "}
+              <span>
+                <b>USDT</b> <span className={styles.no_break}>(ERC-20)</span>
+              </span>
+            </div>,
+            "1 USDT",
+            "25 USDT",
+            "min 1 USDT",
+            "min 10 USDT",
+          ],
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/usdt.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>USDT</b> <span className={styles.no_break}>(TRC-20)</span>
+              </span>
+            </div>,
+            "0,5 USDT",
+            "1.5 USDT",
+            "min 0,5 USDT",
+            "min 10 USDT",
+          ],
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/usdt.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>USDT</b> <span className={styles.no_break}>(TRON)</span>
+              </span>
+            </div>,
+            <span className={styles.text} key={1}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={2}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={3}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={4}>
+              {t("soon")}
+            </span>,
+          ],
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/usdt.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>USDT</b> (SOL)
+              </span>
+            </div>,
+            <span className={styles.text} key={1}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={2}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={3}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={4}>
+              {t("soon")}
+            </span>,
+          ],
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/usdt.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>USDT</b> (BEP-20)
+              </span>
+            </div>,
+            <span className={styles.text} key={1}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={2}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={3}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={4}>
+              {t("soon")}
+            </span>,
+          ],
         ],
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/usdt.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>USDT</b> <span className={styles.no_break}>(TRC-20)</span>
-            </span>
-          </div>,
-          "0,5 USDT",
-          "1.5 USDT",
-          "min 0,5 USDT",
-          "min 10 USDT",
-        ],
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/usdt.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>USDT</b> <span className={styles.no_break}>(TRON)</span>
-            </span>
-          </div>,
-          <span className={styles.text} key={1}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={2}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={3}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={4}>
-            {t("soon")}
-          </span>,
-        ],
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/usdt.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>USDT</b> (SOL)
-            </span>
-          </div>,
-          <span className={styles.text} key={1}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={2}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={3}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={4}>
-            {t("soon")}
-          </span>,
-        ],
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/usdt.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>USDT</b> (BEP-20)
-            </span>
-          </div>,
-          <span className={styles.text} key={1}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={2}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={3}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={4}>
-            {t("soon")}
-          </span>,
-        ],
-      ],
-    },
-    {
-      key: "trx",
-      assets: (
-        <div className={styles.assets}>
-          <div className={classNames(styles.text, styles.subText)}>
-            {t("assets")}
+      },
+      {
+        key: "trx",
+        assets: (
+          <div className={styles.assets}>
+            <div className={classNames(styles.text, styles.subText)}>
+              {t("assets")}
+            </div>
+            <b>TRX </b>Tron
           </div>
-          <b>TRX </b>Tron
-        </div>
-      ),
-      contents: [
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/trx.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>TRX</b> <span className={styles.no_break}>(TRC-20)</span>
-            </span>
-          </div>,
-          "4 TRX",
-          "5 TRX",
-          "min 4 TRX",
-          "min 10 TRX",
+        ),
+        contents: [
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/trx.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>TRX</b> <span className={styles.no_break}>(TRC-20)</span>
+              </span>
+            </div>,
+            "4 TRX",
+            "5 TRX",
+            "min 4 TRX",
+            "min 10 TRX",
+          ],
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/trx.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>TRX</b> <span className={styles.no_break}>(SOL)</span>
+              </span>
+            </div>,
+            <span className={styles.text} key={1}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={2}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={3}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={4}>
+              {t("soon")}
+            </span>,
+          ],
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/trx.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>TRX</b> <span className={styles.no_break}>(BEP-20)</span>
+              </span>
+            </div>,
+            <span className={styles.text} key={1}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={2}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={3}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={4}>
+              {t("soon")}
+            </span>,
+          ],
         ],
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/trx.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>TRX</b> <span className={styles.no_break}>(SOL)</span>
-            </span>
-          </div>,
-          <span className={styles.text} key={1}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={2}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={3}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={4}>
-            {t("soon")}
-          </span>,
-        ],
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/trx.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>TRX</b> <span className={styles.no_break}>(BEP-20)</span>
-            </span>
-          </div>,
-          <span className={styles.text} key={1}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={2}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={3}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={4}>
-            {t("soon")}
-          </span>,
-        ],
-      ],
-    },
-    {
-      key: "eth",
-      assets: (
-        <div className={styles.assets}>
-          <div className={classNames(styles.text, styles.subText)}>
-            {t("assets")}
+      },
+      {
+        key: "eth",
+        assets: (
+          <div className={styles.assets}>
+            <div className={classNames(styles.text, styles.subText)}>
+              {t("assets")}
+            </div>
+            <b>ETH </b>Ethereum
           </div>
-          <b>ETH </b>Ethereum
-        </div>
-      ),
-      contents: [
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/eth.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>ETH</b> <span className={styles.no_break}>(ERC20)</span>
-            </span>
-          </div>,
-          "0,00027 ETH",
-          "0,00625 ETH",
-          "0,00027 ETH",
-          "0,0025 ETH",
+        ),
+        contents: [
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/eth.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>ETH</b> <span className={styles.no_break}>(ERC20)</span>
+              </span>
+            </div>,
+            "0,00027 ETH",
+            "0,00625 ETH",
+            "0,00027 ETH",
+            "0,0025 ETH",
+          ],
         ],
-      ],
-    },
-    {
-      key: "btc",
-      assets: (
-        <div className={styles.assets}>
-          <div className={classNames(styles.text, styles.subText)}>
-            {t("assets")}
+      },
+      {
+        key: "btc",
+        assets: (
+          <div className={styles.assets}>
+            <div className={classNames(styles.text, styles.subText)}>
+              {t("assets")}
+            </div>
+            <b>BTC </b>Bitcoin
           </div>
-          <b>BTC </b>Bitcoin
-        </div>
-      ),
-      contents: [
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/btc.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>Bitcoin</b>
-            </span>
-          </div>,
-          <span className={styles.text} key={1}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={2}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={3}>
-            {t("soon")}
-          </span>,
-          <span className={styles.text} key={4}>
-            {t("soon")}
-          </span>,
+        ),
+        contents: [
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/btc.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>Bitcoin</b>
+              </span>
+            </div>,
+            <span className={styles.text} key={1}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={2}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={3}>
+              {t("soon")}
+            </span>,
+            <span className={styles.text} key={4}>
+              {t("soon")}
+            </span>,
+          ],
         ],
-      ],
-    },
-    {
-      key: "bnb",
-      assets: (
-        <div className={styles.assets}>
-          <div className={classNames(styles.text, styles.subText)}>
-            {t("assets")}
+      },
+      {
+        key: "bnb",
+        assets: (
+          <div className={styles.assets}>
+            <div className={classNames(styles.text, styles.subText)}>
+              {t("assets")}
+            </div>
+            <b>BNB </b>BNB
           </div>
-          <b>BNB </b>BNB
-        </div>
-      ),
-      contents: [
-        [
-          <div className={styles.name} key={0}>
-            <Image
-              className={styles.icon}
-              src="/images/bnb.svg"
-              height={32}
-              width={32}
-              alt="icon"
-            />
-            <span>
-              <b>BNB</b> <span className={styles.no_break}>(BEP-20)</span>
-            </span>
-          </div>,
-          "0,02 BNB",
-          "0,1 BNB",
-          "0,02 BNB",
-          "0,1 BNB",
+        ),
+        contents: [
+          [
+            <div className={styles.name} key={0}>
+              <Image
+                className={styles.icon}
+                src="/images/bnb.svg"
+                height={32}
+                width={32}
+                alt="icon"
+              />
+              <span>
+                <b>BNB</b> <span className={styles.no_break}>(BEP-20)</span>
+              </span>
+            </div>,
+            "0,02 BNB",
+            "0,1 BNB",
+            "0,02 BNB",
+            "0,1 BNB",
+          ],
         ],
-      ],
-    },
-  ];
+      },
+    ],
+    [t]
+  );
 
-  const toggleShow = (key: string) => {
-    console.log(key);
-    setShows((prev) => {
-      if (prev.includes(key)) {
-        return prev.filter((item) => item !== key);
-      }
-      return [...prev, key];
-    });
-  };
+  const toggleShow = useCallback(
+    (key: string) => {
+      setShows((prev) => {
+        if (prev.includes(key)) {
+          return prev.filter((item) => item !== key);
+        }
+        return [...prev, key];
+      });
+    },
+    [setShows]
+  );
+
+  useEffect(() => {
+    setShows(tableContent.map((item) => `${item.key}-0`));
+  }, [tableContent, setShows]);
 
   return (
     <Container
