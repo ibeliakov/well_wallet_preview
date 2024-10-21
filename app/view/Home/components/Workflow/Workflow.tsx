37448c259b5d2313eaef67c0a6f640839cd5d56f
Ivan Bieliakov <ivan.beliakov@data-ox.com>
2024-10-21 12:31:24 +0300

    anumation

--- app/view/Home/components/Workflow/Workflow.tsx
+++ app/view/Home/components/Workflow/Workflow.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { Container, PhoneWithMenu, SectionInfo } from "@/app/components";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 import styles from "./Workflow.module.css";
-import { FC } from "react";
+import { FC, useRef, useState, useEffect } from "react";
+import { useWindowOnEvent } from "@/app/hooks";
 
 const leftList = [
   {
@@ -30,6 +32,51 @@ const rightList = [
 
 const Workflow: FC = () => {
   const t = useTranslations("home");
+  const refPhone = useRef<HTMLDivElement>(null);
+  const chatRef = useRef<HTMLDivElement>(null);
+  const [startAnimation, setStartAnimation] = useState(false);
+
+  useWindowOnEvent(
+    "scroll",
+    () => {
+      const top = refPhone.current?.getBoundingClientRect().top || 0;
+      const height = refPhone.current?.getBoundingClientRect().height || 0;
+      if (
+        top >= 0 &&
+        window.innerHeight -
+          (refPhone.current?.getBoundingClientRect().top || 0) >=
+          height
+      ) {
+        setStartAnimation(true);
+      }
+    },
+    [],
+    true
+  );
+
+  useEffect(() => {
+    if (!startAnimation) {
+      return;
+    }
+    const chat = chatRef.current;
+    const items: ChildNode[] = [];
+
+    chat?.childNodes.forEach((item) => {
+      items.push(item);
+    });
+    let i = 0;
+    //@ts-ignore
+    chat?.parentElement.classList.add("active")
+    const interval = setInterval(() => {
+      //@ts-ignore
+      items[i].classList.add("active");
+      i++;
+      if (i === items.length) {
+        clearInterval(interval);
+        return;
+      }
+    }, 1000);
+  }, [startAnimation]);
 
   return (
     <section className={styles.wrap}>
@@ -51,12 +98,14 @@ const Workflow: FC = () => {
           ))}
         </div>
         <div className={styles.example}>
-          <div className={styles.chat}>
+          <div ref={chatRef} className={styles.chat}>
             <div className={styles.today}>Today</div>
             <div className={styles.start}>/start</div>
             <div className={styles.welcome}>{t("welcome")}</div>
           </div>
-          <PhoneWithMenu className={styles.phone} />
+          <div ref={refPhone}>
+            <PhoneWithMenu className={styles.phone} />
+          </div>
         </div>
         <div className={styles.column}>
           {rightList.map(({ name, image }, i) => (
