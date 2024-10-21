c18799ce0d088db3ecaf2eadbf5925f9fada5dcb
Ivan Bieliakov <ivan.beliakov@data-ox.com>
2024-10-21 05:53:54 +0300

    remobe extra code

--- app/components/Footer/components/MainInfo/MainInfo.tsx
+++ app/components/Footer/components/MainInfo/MainInfo.tsx
@@ -1,12 +1,10 @@
 import { Logo } from "@/app/components";
-import { useTranslations } from "next-intl";
 import { FC } from "react";
 
 import styles from "./MainInfo.module.css";
 import ColonTitle from "../ColonTitle";
 
 const MainInfo: FC = () => {
-  const t = useTranslations("common");
   return (
     <div className={styles.box}>
       <Logo zoom={1.11} />
